9a1b061e1b213ec3fc67397b8e7e5f2efcff3019
SGVRDiLin <D.Lin3@student.gsa.ac.uk>
2025-03-18 02:59:11 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# CppGOWActionGame
+ 
---
+++ Source/CppGOWActionGame/CppGOWActionGame.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CppGOWActionGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CppGOWActionGame, "CppGOWActionGame" );

ccd48bed7e47de840dbd8e6e375eff7c0fab9e90
DilinDelacrux <674092582@qq.com>
2025-03-19 09:13:45 +0000

    import 3person and C++ class

---
+++ Source/CppGOWActionGame/Public/GameMode/GWOGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GWOGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CPPGOWACTIONGAME_API AGWOGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/CppGOWActionGame/Public/Misc/WarriorDebugHelper.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+
+namespace Debug
+{
+	static void Print(const FString &str,const FColor& color=FColor::MakeRandomColor(),int inKey=-1)
+	{
+		if(GEngine)
+		{
+			GEngine->AddOnScreenDebugMessage(inKey,7.f,color,str);
+			UE_LOG(LogTemp,Warning,TEXT("%s"),*str);
+		}
+	}
+}

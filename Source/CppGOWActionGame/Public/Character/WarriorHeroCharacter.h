1f933b8ce47d375255b0448962b1fa6f38ea1843
DilinDelacrux <674092582@qq.com>
2025-03-19 09:42:38 +0000

    CppNativeGametag

--- Source/CppGOWActionGame/Public/Character/WarriorHeroCharacter.h
+++ Source/CppGOWActionGame/Public/Character/WarriorHeroCharacter.h
@@ -6,6 +6,8 @@
 #include "Character/WarriorBaseCharacter.h"
 #include "WarriorHeroCharacter.generated.h"
 
+class UCameraComponent;
+class USpringArmComponent;
 /**
  * 
  */
@@ -13,5 +15,18 @@ UCLASS()
 class CPPGOWACTIONGAME_API AWarriorHeroCharacter : public AWarriorBaseCharacter
 {
 	GENERATED_BODY()
+
+protected:
+	virtual void BeginPlay() override;
+public:
+	AWarriorHeroCharacter();
 	
+private:
+#pragma region component
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly,Category="Camera",meta=(AllowPrivateAccess=true))
+	USpringArmComponent* CameraBoom;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly,Category="Camera",meta=(AllowPrivateAccess=true))
+	UCameraComponent* FollowCamera;
+	
+#pragma endregion
 };

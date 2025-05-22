5a9df3d1507e50e169d6865aa21293a32d22b7ea
DilinDelacrux <674092582@qq.com>
2025-05-22 10:11:39 +0100

    Equip Weapon

--- Source/CppGOWActionGame/Public/AbilitySystem/Abilities/WarriorHeroGameplayAbility.h
+++ Source/CppGOWActionGame/Public/AbilitySystem/Abilities/WarriorHeroGameplayAbility.h
@@ -28,5 +28,5 @@ public:
 	
 private:
 	 TWeakObjectPtr<AWarriorHeroCharacter> CachedHeroCharacter;
-	 TWeakObjectPtr<AWarriorHeroController> CachedHeroController;
+	 TWeakObjectPtr<AWarriorHeroController> CachedHeroController; 
 };
--- Source/CppGOWActionGame/Public/Character/WarriorHeroCharacter.h
+++ Source/CppGOWActionGame/Public/Character/WarriorHeroCharacter.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
 #include "InputActionValue.h"
 #include "Character/WarriorBaseCharacter.h"
 #include "WarriorHeroCharacter.generated.h"
@@ -44,6 +45,8 @@ private:
 
 	void Input_Move(const FInputActionValue& InputActionValue);
 	void Input_Look(const FInputActionValue& InputActionValue);
+	void Input_AbilityInputPressed(FGameplayTag InInputTag);
+	void Input_AbilityInputReleased(FGameplayTag InInputTag);
 	
 	
 #pragma endregion 

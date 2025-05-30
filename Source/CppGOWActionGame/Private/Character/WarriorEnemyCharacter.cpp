bb07824b8f9697b4b5c3c738c8ffcecc9f4ca932
DilinDelacrux <674092582@qq.com>
2025-05-30 13:59:09 +0100

    Apply Attribute2

---
+++ Source/CppGOWActionGame/Private/Character/WarriorEnemyCharacter.cpp
@@ -0,0 +1,50 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Character/WarriorEnemyCharacter.h"
+#include "Engine/AssetManager.h"
+#include "Components/EnemyCombatComponent.h"
+#include "DataAssets/StartUpData/DataAsset_StartUpDataBase.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Misc/WarriorDebugHelper.h"
+
+AWarriorEnemyCharacter::AWarriorEnemyCharacter()
+{
+	AutoPossessAI=EAutoPossessAI::PlacedInWorldOrSpawned;
+	bUseControllerRotationPitch=false;
+	bUseControllerRotationYaw=false;
+	bUseControllerRotationRoll=false;
+
+	GetCharacterMovement()->bUseControllerDesiredRotation=false;
+	GetCharacterMovement()->bOrientRotationToMovement=true;
+	GetCharacterMovement()->RotationRate=FRotator(0.0f,180.0f,0.0f);
+	GetCharacterMovement()->MaxWalkSpeed=300.f;
+	GetCharacterMovement()->BrakingDecelerationWalking=1000.f;
+
+	CombatComponent=CreateDefaultSubobject<UEnemyCombatComponent>("EnemyCombatComponent");
+}
+
+void AWarriorEnemyCharacter::PossessedBy(AController* NewController)
+{
+	Super::PossessedBy(NewController);
+	InitEnemySetupData();
+}
+
+void AWarriorEnemyCharacter::InitEnemySetupData()
+{
+	if(CharacterStartUpData.IsNull()) return;
+	UAssetManager::GetStreamableManager().RequestAsyncLoad(
+		CharacterStartUpData.ToSoftObjectPath(),
+
+		FStreamableDelegate::CreateLambda([this]()
+		{
+			if(UDataAsset_StartUpDataBase* LoadedData=CharacterStartUpData.Get())
+			{
+				LoadedData->GiveToAbilitySystemComponent(WarriorAbilitySystemComponent);
+				Debug::Print(TEXT("EnemyStartUpDataLoaded"),FColor::Green);
+			}
+		})
+		);
+
+	
+}
---
+++ Source/CppGOWActionGame/Public/Character/WarriorEnemyCharacter.h
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/WarriorBaseCharacter.h"
+#include "WarriorEnemyCharacter.generated.h"
+
+class UEnemyCombatComponent;
+/**
+ * 
+ */
+UCLASS()
+class CPPGOWACTIONGAME_API AWarriorEnemyCharacter : public AWarriorBaseCharacter
+{
+	GENERATED_BODY()
+
+public:
+	AWarriorEnemyCharacter();
+	virtual void PossessedBy(AController* NewController) override;
+
+
+protected:
+	UPROPERTY(VisibleAnywhere,BlueprintReadOnly,Category="Combat")
+	UEnemyCombatComponent* CombatComponent;
+
+public:
+	FORCEINLINE UEnemyCombatComponent* GetCombatComponent() const { return CombatComponent; }
+
+private:
+	void InitEnemySetupData();
+	
+};

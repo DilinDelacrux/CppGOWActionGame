26b2e2c1fda7de83faaea14c3905c6a90a384545
DilinDelacrux <674092582@qq.com>
2026-08-20 15:37:26 +0800

    Stagger ambient NPC daily schedules

--- Content/JavaScript/AmbientNpcDailyScheduleDemo.js
+++ Content/JavaScript/AmbientNpcDailyScheduleDemo.js
@@ -45,11 +45,11 @@ class AmbientNpcDailyScheduleDemo extends UE.BehaviorFrameworkManagerBase {
         schedule.activePairs = pairs;
     }
     stateAt(npc, hour) {
-        if (hour >= 12 && hour < 13)
+        if (hour >= npc.lunchStart && hour < npc.lunchStart + 1)
             return '午饭';
-        if (hour >= 18 && hour < 19)
+        if (hour >= npc.dinnerStart && hour < npc.dinnerStart + 1)
             return '晚饭';
-        if ((hour >= npc.workStart && hour < npc.workEnd) || (hour >= 13 && hour < 17))
+        if ((hour >= npc.workStart && hour < npc.lunchStart) || (hour >= npc.lunchStart + 1 && hour < npc.workEnd))
             return '工作';
         if (hour >= npc.relaxStart && hour < npc.relaxEnd)
             return '放松';
@@ -70,16 +70,16 @@ class AmbientNpcDailyScheduleDemo extends UE.BehaviorFrameworkManagerBase {
                 lastLoggedSecond: -1,
                 activePairs: new Set(),
                 npcs: [
-                    { name: '阿诺', job: '铁匠', workStart: 6, workEnd: 12, relaxStart: 17, relaxEnd: 22 },
-                    { name: '贝拉', job: '裁缝', workStart: 7, workEnd: 12, relaxStart: 16, relaxEnd: 21 },
-                    { name: '科尔', job: '面包师', workStart: 4, workEnd: 11, relaxStart: 15, relaxEnd: 20 },
-                    { name: '黛西', job: '药剂师', workStart: 8, workEnd: 12, relaxStart: 18, relaxEnd: 22 },
-                    { name: '埃德', job: '木匠', workStart: 6, workEnd: 12, relaxStart: 17, relaxEnd: 21 },
-                    { name: '菲奥娜', job: '陶工', workStart: 8, workEnd: 12, relaxStart: 16, relaxEnd: 22 },
-                    { name: '加文', job: '猎人', workStart: 5, workEnd: 11, relaxStart: 17, relaxEnd: 20 },
-                    { name: '海伦', job: '旅店老板', workStart: 9, workEnd: 12, relaxStart: 15, relaxEnd: 21 },
-                    { name: '伊万', job: '农夫', workStart: 5, workEnd: 12, relaxStart: 16, relaxEnd: 20 },
-                    { name: '朱恩', job: '书记员', workStart: 8, workEnd: 12, relaxStart: 18, relaxEnd: 22 },
+                    { name: '阿诺', job: '铁匠', workStart: 6.0, lunchStart: 11.8, workEnd: 16.9, dinnerStart: 18.0, relaxStart: 16.9, relaxEnd: 21.8 },
+                    { name: '贝拉', job: '裁缝', workStart: 7.2, lunchStart: 12.0, workEnd: 16.7, dinnerStart: 18.2, relaxStart: 16.7, relaxEnd: 21.0 },
+                    { name: '科尔', job: '面包师', workStart: 4.8, lunchStart: 11.6, workEnd: 15.8, dinnerStart: 17.8, relaxStart: 15.8, relaxEnd: 20.2 },
+                    { name: '黛西', job: '药剂师', workStart: 7.8, lunchStart: 12.2, workEnd: 17.2, dinnerStart: 18.4, relaxStart: 17.2, relaxEnd: 22.0 },
+                    { name: '埃德', job: '木匠', workStart: 6.2, lunchStart: 11.9, workEnd: 17.0, dinnerStart: 18.1, relaxStart: 17.0, relaxEnd: 21.2 },
+                    { name: '菲奥娜', job: '陶工', workStart: 8.0, lunchStart: 12.1, workEnd: 16.6, dinnerStart: 18.3, relaxStart: 16.6, relaxEnd: 21.9 },
+                    { name: '加文', job: '猎人', workStart: 5.1, lunchStart: 11.7, workEnd: 16.8, dinnerStart: 17.9, relaxStart: 16.8, relaxEnd: 20.1 },
+                    { name: '海伦', job: '旅店老板', workStart: 8.8, lunchStart: 12.3, workEnd: 16.2, dinnerStart: 18.0, relaxStart: 16.2, relaxEnd: 21.1 },
+                    { name: '伊万', job: '农夫', workStart: 5.0, lunchStart: 11.9, workEnd: 16.5, dinnerStart: 18.2, relaxStart: 16.5, relaxEnd: 20.0 },
+                    { name: '朱恩', job: '书记员', workStart: 8.1, lunchStart: 12.2, workEnd: 17.1, dinnerStart: 18.4, relaxStart: 17.1, relaxEnd: 22.0 },
                 ],
             };
             scheduleStates.set(this, state);

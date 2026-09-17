0a2374d9f56b87f8bfcb10560f2381ee95842f66
DilinDelacrux <674092582@qq.com>
2026-09-17 15:14:48 +0800

    refactor: 移除过度设计的 AmbientNpcBehavior 插件及相关演示代码

--- Plugins/AmbientNpcBehavior/Source/AmbientNpcBehavior/Private/BehaviorFrameworkConfig.cpp
+++
@@ -1,4 +0,0 @@
-#include "BehaviorFrameworkConfig.h"
-
-// UBehaviorFrameworkConfig is a plain data asset; no additional implementation
-// is required beyond the UPROPERTY declarations in the header.
--- Plugins/AmbientNpcBehavior/Source/AmbientNpcBehavior/Private/Framework/src/services/layers/0_foundation/SelectionAlgorithmOption.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-namespace AmbientCharacterBehavior {
-enum class SelectionAlgorithmOption {
-    MEMORY_BASED = 0,
-    UNIFORM_RANDOM = 1,
-};
-}
-
